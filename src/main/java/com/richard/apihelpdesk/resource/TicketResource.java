9270977f96b139948c93736adc346d8a7fa63e57
Ricardolv <richardluizv@gmail.com>
2018-05-05 13:10:12 -0300

    criando controller ticket

---
+++ src/main/java/com/richard/apihelpdesk/resource/TicketResource.java
@@ -0,0 +1,91 @@
+package com.richard.apihelpdesk.resource;
+
+import java.util.Date;
+import java.util.Random;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.richard.apihelpdesk.entity.Ticket;
+import com.richard.apihelpdesk.entity.User;
+import com.richard.apihelpdesk.entity.enums.StatusEnum;
+import com.richard.apihelpdesk.resource.response.Response;
+import com.richard.apihelpdesk.security.jwt.JwtTokenUtil;
+import com.richard.apihelpdesk.service.TicketService;
+import com.richard.apihelpdesk.service.UserService;
+
+@RestController
+@RequestMapping("/api/ticket")
+@CrossOrigin(origins = "*")
+public class TicketResource {
+	
+	@Autowired
+	private TicketService ticketService;
+	
+	@Autowired
+	protected JwtTokenUtil jwtTokenUtil;
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping
+	@PreAuthorize("hasAnyRole('ADMIN','CUSTOMER', 'TECHNICIAN')")
+	public ResponseEntity<Response<Ticket>> createOrUpdate(HttpServletRequest request, @RequestBody Ticket ticket, BindingResult result) {
+		Response<Ticket> response = new Response<>();
+		
+		try {
+			validateCreateTicket(ticket, result);
+			
+			if (result.hasErrors()) {
+				result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+				return ResponseEntity.badRequest().body(response);
+			}
+			
+			ticket.setStatus(StatusEnum.getStatus("New"));
+			ticket.setUser(userFromRequest(request));
+			ticket.setDate(new Date());
+			ticket.setNumber(generateNumber());
+			
+			Ticket ticketPersisted = (Ticket) ticketService.createOrUpdate(ticket);
+			response.setData(ticketPersisted);
+			
+		} catch (Exception e) {
+			response.getErrors().add(e.getMessage());
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+
+	private void validateCreateTicket(Ticket ticket, BindingResult result) {
+		
+		if (StringUtils.isBlank(ticket.getTitle())) {
+			result.addError(new ObjectError("Ticket", "Title no information"));
+		}
+		
+	}
+	
+	private User userFromRequest(HttpServletRequest request) {
+		String token = request.getHeader("Authorization");
+		String email = jwtTokenUtil.getUserNameFromToken(token);
+		return userService.findByEmail(email).get();
+	}
+	
+	private Integer generateNumber() {
+		Random random = new Random();
+		return random.nextInt(9999);
+	}
+
+}
